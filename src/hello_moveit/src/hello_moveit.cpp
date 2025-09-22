349f8ebd6492d01c7cabbdee73a3365dbb983e47
YuuuuuQingChi <1400982623@qq.com>
2025-09-22 22:33:32 +0800

    1

--- src/hello_moveit/src/hello_moveit.cpp
+++ src/hello_moveit/src/hello_moveit.cpp
@@ -1,44 +1,107 @@
-#include "geometry_msgs/msg/pose.hpp"
-#include "memory"
-#include "moveit/move_group_interface/move_group_interface.hpp"
-#include "rclcpp/rclcpp.hpp"
-#include <memory>
-#include <rclcpp/logger.hpp>
-#include <rclcpp/logging.hpp>
-#include <rclcpp/node.hpp>
-#include <rclcpp/node_options.hpp>
-#include <rclcpp/utilities.hpp>
+#include <moveit/move_group_interface/move_group_interface.hpp>
+#include <moveit_visual_tools/moveit_visual_tools.h>
 
-int main(int argc, char **argv) {
+#include <memory>
+#include <rclcpp/rclcpp.hpp>
+#include <thread>
+
+int main(int argc, char *argv[]) {
+  // Initialize ROS and create the Node
   rclcpp::init(argc, argv);
   auto const node = std::make_shared<rclcpp::Node>(
       "hello_moveit",
       rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(
           true));
 
-  auto const logger = rclcpp::get_logger("1212");
+  // Create a ROS logger
+  auto const logger = rclcpp::get_logger("hello_moveit");
 
+  // We spin up a SingleThreadedExecutor for the current state monitor to get
+  // information about the robot's state.
+  rclcpp::executors::SingleThreadedExecutor executor;
+  executor.add_node(node);
+  auto spinner = std::thread([&executor]() { executor.spin(); });
+
+  // Create the MoveIt MoveGroup Interface
   using moveit::planning_interface::MoveGroupInterface;
-  auto move_group_interface = MoveGroupInterface(node,"manipulator");
+  auto move_group_interface = MoveGroupInterface(node, "manipulator");
+
+  // Construct and initialize MoveItVisualTools
+  auto moveit_visual_tools = moveit_visual_tools::MoveItVisualTools{
+      node, "base_link", rviz_visual_tools::RVIZ_MARKER_TOPIC,
+      move_group_interface.getRobotModel()};
+  // 我们调用删除所有标记的函数。
+  moveit_visual_tools.deleteAllMarkers();
+  // 加载远程控制插件
+  moveit_visual_tools.loadRemoteControl();
+
+  // 定义一个常量Lambda函数draw_title
+  // 捕获外部变量moveit_visual_tools（按引用）
+  // 接受一个自动类型参数text（字符串）
+  auto const draw_title = [&moveit_visual_tools](auto text) {
+    auto const text_pose = [] {
+      auto msg = Eigen::Isometry3d::Identity();//创造三维那个空间变换矩阵
+      msg.translation().z() = 1.0; // Place text 1m above the base link
+      return msg;
+    }();
+    //带一个（）是即刻执行的意思
+
+    //这个是在rviz场景中打印出text
+    moveit_visual_tools.publishText(text_pose, text, rviz_visual_tools::WHITE,
+                                    rviz_visual_tools::XLARGE);
+  };
+
+  //在MoveIt中，VisualTools（可视化工具）是一个用于在Rviz中显示各种标记（如机器人模型、路径、文本等）的实用工具库。其中，prompt方法用于在Rviz中显示一个提示信息，并等待用户响应（例如按下键盘上的某个键）。
+  auto const prompt = [&moveit_visual_tools](auto text) {
+    moveit_visual_tools.prompt(text);
+  };
+
+  //prompt会阻塞任务直到用户按了next
+  auto const draw_trajectory_tool_path =
+      [&moveit_visual_tools,
+       jmg = move_group_interface.getRobotModel()->getJointModelGroup(
+           "manipulator")](auto const trajectory) {
+        moveit_visual_tools.publishTrajectoryLine(trajectory, jmg);
+      };
+
+  // Set a target Pose
   auto const target_pose = [] {
     geometry_msgs::msg::Pose msg;
+    msg.orientation.w = 1.0;
     msg.position.x = 0.28;
     msg.position.y = -0.2;
     msg.position.z = 0.5;
-
     return msg;
   }();
   move_group_interface.setPoseTarget(target_pose);
-  move_group_interface.setRPYTarget(0.0,1.0,0.0);
 
-  auto const [success, plan] = [&move_group_interface]{
-  moveit::planning_interface::MoveGroupInterface::Plan msg;
-  auto const ok = static_cast<bool>(move_group_interface.plan(msg));
-  return std::make_pair(ok, msg);
- 
+  // Create a plan to that target pose
+  prompt("Press 'next' in the RvizVisualToolsGui window to plan");
+  draw_title("Planning");
+  moveit_visual_tools.trigger();
+  auto const [success, plan] = [&move_group_interface] {
+    moveit::planning_interface::MoveGroupInterface::Plan msg;
+    auto const ok = static_cast<bool>(move_group_interface.plan(msg));
+    return std::make_pair(ok, msg);
+  }();
 
+  // Execute the plan
+  if (success) {
+    draw_trajectory_tool_path(plan.trajectory);
+    //在MoveIt的visual_tools中，trigger()方法是一个重要的工具，用于立即发布所有累积的可视化标记。
+    moveit_visual_tools.trigger();
+    prompt("Press 'next' in the RvizVisualToolsGui window to execute");
+    draw_title("Executing");
+    moveit_visual_tools.trigger();
+    move_group_interface.execute(plan);
+  } else {
+    draw_title("Planning Failed!");
+    moveit_visual_tools.trigger();
+    RCLCPP_ERROR(logger, "Planning failed!");
+  }
 
-}();
+  // Shutdown ROS
   rclcpp::shutdown();
+  spinner.join();
   return 0;
 }
